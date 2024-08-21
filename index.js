f13f031f91cbde09e0b4db066099c8689ffc0fce
manoj <sen2mksingh@gmail.com>
2024-08-21 11:45:56 +0000

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => res.send('Hello, Jenkins!'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
 
